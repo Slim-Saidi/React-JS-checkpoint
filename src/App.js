01273979a99f3b2ecfae70f945e968480fabd780
slim-saidi <slim-saidi@hotmail.com>
2020-11-25 11:35:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FormComp from "./Form"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FormComp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
